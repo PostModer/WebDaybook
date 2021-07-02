a132ea1aa8fb097c381c4850225247857efb5bc6
MAX L <Maximum00214@yandex.ru>
2021-07-02 11:37:42 +0300

    Сделан проект до 398 стр.

---
+++ learning_log/learning_logs/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Topic(models.Model):
+    '''Настройка топика'''
+
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+
+class Entry(models.Model):
+    '''Инфа, изученная пользователем по теме'''
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        if len(self.text) >= 49:
+            return self.text[:50] + '...'
+        else:
+            return self.text
+
+
+
+
---
+++ learning_log/learning_logs/urls.py
@@ -0,0 +1,10 @@
+"""Определяет схемы URL для learning_logs"""
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    # Домашняя страница
+    url(r'^$', views.index, name='index'),
+]
